359f68e8a369072c29279a0e91c07ce23a6d9882
Miroslav Svoboda <miroslav.svoboda@rein-it.com>
2019-06-30 21:32:21 +0200

    Fix CORS

--- src/main/java/com/karumien/cloud/ais/api/AISWorkRestController.java
+++ src/main/java/com/karumien/cloud/ais/api/AISWorkRestController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -321,6 +322,7 @@ public class AISWorkRestController implements WorkApi {
      * {@inheritDoc}
      */
     @Override
+    @CrossOrigin(origins = "*")
     public ResponseEntity<Void> setWork(@Valid WorkDTO work, @NotNull @Valid String username) {
         aisService.setWork(work, username);
         return new ResponseEntity<>(HttpStatus.OK);
