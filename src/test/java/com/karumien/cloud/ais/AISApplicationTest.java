e4882b23825c6989261cbc5a3309bceabeccbe5a
Miroslav Svoboda <miroslav.svoboda@karumien.com>
2019-06-30 12:04:44 +0200

    Ignore tests

--- src/test/java/com/karumien/cloud/ais/AISApplicationTest.java
+++ src/test/java/com/karumien/cloud/ais/AISApplicationTest.java
@@ -7,18 +7,19 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.karumien.cloud.ais.api.entity.UserInfo;
 import com.karumien.cloud.ais.api.model.UserInfoDTO;
 import com.karumien.cloud.ais.api.model.WorkMonthDTO;
 import com.karumien.cloud.ais.exception.NoDataFoundException;
 import com.karumien.cloud.ais.service.AISService;
 
+@Ignore
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AISApplicationTest {
