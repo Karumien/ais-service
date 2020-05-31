0315794a75e608e1a95d7611e9cd37316da0ad6a
Miroslav Svoboda <miroslav.svoboda@rein-it.com>
2020-06-01 00:04:49 +0200

    Add 6:00

--- src/main/java/com/karumien/cloud/ais/api/HomeController.java
+++ src/main/java/com/karumien/cloud/ais/api/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
     
     @RequestMapping(value = "/")
     public String index() {
-        System.out.println("swagger-ui.html");
+//        System.out.println("swagger-ui.html");
         return "redirect:swagger-ui.html";
     }
     
